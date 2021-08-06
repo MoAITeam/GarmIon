09890480c5c2b375d74d139114a5acb1a6b7239c
Marco Guerra <marco.guerra@stud.unifi.it>
2021-08-06 16:08:32 +0200

    rest udpate

--- backend/rest/models.py
+++ backend/rest/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Picture(models.Model):
     picId = models.CharField(max_length=60)
-    image = models.ImageField(upload_to ='./')
+    image = models.ImageField(upload_to ='uploads/')
     def __str__(self):
         return self.picId
