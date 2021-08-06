64c4667c6e408788fd5960c8d37601ead1eef385
Marco Guerra <marco.guerra@stud.unifi.it>
2021-08-06 15:58:42 +0200

    rest framework finito

--- backend/rest/models.py
+++ backend/rest/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Picture(models.Model):
     picId = models.CharField(max_length=60)
-    image = models.ImageField(upload_to ='uploads/')
+    image = models.ImageField(upload_to ='./')
     def __str__(self):
         return self.picId
