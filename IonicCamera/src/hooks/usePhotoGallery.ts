94cce8fabab4ff6f03df626d30123921e93963d6
Marco Guerra <marco.guerra@stud.unifi.it>
2021-08-10 03:01:18 +0200

    list

--- IonicCamera/src/hooks/usePhotoGallery.ts
+++ IonicCamera/src/hooks/usePhotoGallery.ts
@@ -32,35 +32,18 @@ export function usePhotoGallery() {
         // prints 201
         console.log(response.status);
         let responsed = JSON.parse(response.data);
-        let path = responsed['recommended garments'][0]['image'];
-        HTTP.sendRequest('http://192.168.1.190:8000/'+path,
-      {
-        method: 'get',
-        //headers: { Authorization: 'OAuth2: token' },
-        timeout: 5000
-      }
-    )
-      .then(response1 => {
-        // prints 201
-        console.log(response1.status);
-        console.log(response1.data);
-        let downloaded_picture = response1.data;
         const newPhotos = [
-    {
-      filepath: fileName,
-      webviewPath: 'http://192.168.1.190:8000/'+path,
-    },
-    ...photos,
-    ];
-    setPhotos(newPhotos);
-      })
-      .catch(response1 => {
-        // prints 403
-        console.log(response1);
-
-        // prints Permission denied
-        console.log(response1);
-      });
+          {
+            filepath: fileName,
+            webviewPath: 'http://192.168.1.190:8000/'+responsed['recommended garments'][0]['image'],
+          },
+          {
+            filepath: fileName,
+            webviewPath: 'http://192.168.1.190:8000/'+responsed['recommended garments'][1]['image'],
+          },
+          ...photos,
+          ];
+          setPhotos(newPhotos);;
       })
       .catch(response => {
         // prints 403
--- IonicCamera/src/pages/Tab1.tsx
+++ IonicCamera/src/pages/Tab1.tsx
@@ -1,8 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonList, IonItemDivider, IonListHeader, IonLabel, IonItem, IonSelect, IonSelectOption} from '@ionic/react';
+import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const [event_type, setEvent] = useState<string>();
+  const [mood, setMood] = useState<string>('happy'); 
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +20,32 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <IonList>
+          <IonListHeader>
+            <IonLabel>
+              Single Selection
+            </IonLabel>
+          </IonListHeader>
+
+          <IonItem>
+            <IonLabel>Event</IonLabel>
+            <IonSelect value={event_type} placeholder="Select Event" onIonChange={e => setEvent(e.detail.value)}>
+              <IonSelectOption value="wedding">Wedding</IonSelectOption>
+              <IonSelectOption value="funeral">Funeral</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+
+          <IonItem>
+            <IonLabel>Mood</IonLabel>
+            <IonSelect value={mood} okText="Okay" cancelText="Dismiss" onIonChange={e => setMood(e.detail.value)}>
+              <IonSelectOption value="happy">Happpy</IonSelectOption>
+              <IonSelectOption value="sad">Sad</IonSelectOption>
+            </IonSelect>
+          </IonItem>
+          <IonItemDivider>Your Selections</IonItemDivider>
+          <IonItem>Event: {event_type ?? '(none selected)'}</IonItem>
+          <IonItem>Mood: {mood}</IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
