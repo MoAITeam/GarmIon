48c67291ee5e3cb35f4e47b07269750707c0245c
Marco Guerra <marco.guerra@stud.unifi.it>
2021-09-04 18:43:22 +0200

    ora funziona l'immagine in outfit
    sembra che non si può usare [0] nella sintassi angular html

--- mockup/src/app/outfit-detail/outfit-detail.component.ts
+++ mockup/src/app/outfit-detail/outfit-detail.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Outfit } from '../outfit/outfit.component';
 import { OUTFITS } from '../outfit-mockup';
+import { Garment } from '../garment';
 
 @Component({
   selector: 'app-outfit-detail',
@@ -13,6 +14,8 @@ import { OUTFITS } from '../outfit-mockup';
 export class OutfitDetailComponent implements OnInit {
 
   public outfit:Outfit;
+  public userGarment:Garment;
+  public recommendedGarment:Garment;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,6 +25,9 @@ export class OutfitDetailComponent implements OnInit {
   ngOnInit() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.outfit = OUTFITS.find(h => h.id === id)!;
+    this.userGarment = this.outfit.garments[0];
+    this.recommendedGarment = this.outfit.garments[1];
+
   }
 
 }
