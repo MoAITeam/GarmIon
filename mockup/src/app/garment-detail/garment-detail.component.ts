d733bc1feb16f850b1a2bcd1dee1d4b27aab746b
Marco Guerra <marco.guerra@stud.unifi.it>
2021-09-06 20:18:32 +0200

    funziona tutto

--- mockup/src/app/garment-detail/garment-detail.component.ts
+++ mockup/src/app/garment-detail/garment-detail.component.ts
@@ -31,18 +31,19 @@ export class GarmentDetailComponent implements OnInit {
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.garment = GARMENTS.find(h => h.id === id)!;
-    this.matchGarments = [{
-      id: 1, name: 'TeslaTits', link: 'https://www.net-a-porter.com/variants/images/6630340699385535/in/w2000.jpg', color: 'Red',category:'bottom' },
+    this.lovedOutfit = [];
+    this.matchGarments = [
+    {  id: 1, name: 'TeslaTits', link: 'https://www.net-a-porter.com/variants/images/6630340699385535/in/w2000.jpg', color: 'Red',category:'bottom' },
     { id: 2, name: 'Cosciona99', link: 'https://www.net-a-porter.com/variants/images/6630340699385535/in/w2000.jpg',color: 'Blue',category:'top' },
     { id: 3, name: 'Banana33', link: 'https://www.net-a-porter.com/variants/images/6630340699385535/in/w2000.jpg',color: 'Red',category:'top' }
-  ];
-}
+    ];
+    }
 
   save(){
     let async_id = this.sliderComponent.getActiveIndex();
 
     async_id.then( id => {
-      let lovedMatch = this.matchGarments.find(h=>h.id===id)!;
+      let lovedMatch = this.matchGarments.find(h=>h.id===(id+1))!;
       let randomID:number = + Math.random().toString(36).substr(2, 9);
       let outfitToSave = {id: randomID, userGarment: this.garment, matchGarment: lovedMatch};
       this.lovedOutfit.push(outfitToSave);
@@ -63,11 +64,11 @@ export class GarmentDetailComponent implements OnInit {
 
   slideChange() {
     let async_id = this.sliderComponent.getActiveIndex();
-  
+    console.log("slide changed");
 
     async_id.then( id => {
-      let lovedMatch = this.matchGarments.find(h=>h.id===id)!;
-      let findOutfit = this.lovedOutfit.find(v=>v.matchGarment.id===id)
+      let lovedMatch = this.matchGarments.find(h=>h.id===(id+1))!;
+      let findOutfit = this.lovedOutfit.find(v=>v.matchGarment.id===(id+1));
       let icon = document.getElementById('heart-icon');
       if (findOutfit){
         icon.setAttribute('name','heart');
