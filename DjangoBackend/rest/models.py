c1d111be73f0ac76ae1192a3ded64d1fdb535d66
Marco Guerra <ciuffysrise@gmail.com>
2024-02-08 00:31:45 -0800

    Delete DjangoBackend directory

--- DjangoBackend/rest/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class Picture(models.Model):
-    picId = models.CharField(max_length=60)
-    image = models.ImageField(upload_to ='uploads/')
-    def __str__(self):
-        return self.picId
-
-
